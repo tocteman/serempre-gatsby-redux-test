98d51a1aad068da8ec609303c210d55ee3440e4e
tocteman <juanfrancisco.vinueza@gmail.com>
2020-10-03 23:49:46 -0500

    arrancamos

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-red-500 text-5xl font-bold text-center">
+      a ver
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      "sans": ["'nort'", "sans-serif"],
+      "display": ["nort", "sans-serif"],
+      "body": ["nort", "sans-serif"]
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
